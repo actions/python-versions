33383986c900f38830642ae6f215804f5cb66098
Maksim Petrov <v-mapetr@microsoft.com>
2020-06-04 19:13:53 +0300

    Fix in test

--- tests/sources/python-sqlite3.py
+++ tests/sources/python-sqlite3.py
@@ -1,13 +1,13 @@
 import sqlite3
 from sqlite3 import Error
 
-
 def create_connection(db_file):
     """ create a database connection to a SQLite database """
     conn = None
     try:
-        conn = sqlite3.connect(db_file)
         print(sqlite3.version)
+        conn = sqlite3.connect(db_file)
+        conn.enable_load_extension(True)
     except Error as e:
         print(e)
         exit(1)
