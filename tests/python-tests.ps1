ff4ca0b2e5b1096da59f305868b1961a8c145b56
Dmitry Shibanov <shibanov-1997@inbox.ru>
2020-10-29 10:39:08 +0300

    skip test for python 2 (#60)
    Co-authored-by: Dmitry Shibanov <v-dmshib@microsoft.com>

--- tests/python-tests.ps1
+++ tests/python-tests.ps1
@@ -75,7 +75,7 @@ Describe "Tests" {
     }
 
     # Pyinstaller 3.5 does not support Python 3.8.0. Check issue https://github.com/pyinstaller/pyinstaller/issues/4311
-    if ($Version -lt "3.8.0") {
+    if ($Version -lt "3.8.0" -and $Version.Major -ne "2") {
         It "Validate Pyinstaller" {
             "pip install pyinstaller" | Should -ReturnZeroExitCode
             "pyinstaller --onefile ./sources/simple-test.py" | Should -ReturnZeroExitCode
