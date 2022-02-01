c5bb3e036011f112bfbda60da123725ddd9d638f
Maksim Shilov <maksim.shilov@akvelon.com>
2022-02-01 10:38:54 +0300

    resolve comments

--- builders/python-versions-runner.ps1
+++ builders/python-versions-runner.ps1
@@ -8,29 +8,36 @@ Script that triggering and fetching the result of the "Build python package" wor
 .PARAMETER Version
 Required parameter. Python version to trigger build for.
 
+.PARAMETER PublishRelease
+Switch parameter. Whether to publish release for built version.
+
 #>
 
 param(
     [Parameter (Mandatory=$true, HelpMessage="Python version to trigger build for")]
-    [string] $Version
+    [string] $Version,
+    [Parameter (Mandatory=$false, HelpMessage="Whether to publish release for built version")]
+    [switch] $PublishRelease
 )
 
 $payload = @{
     "ref" = "main"
     "inputs" = @{
         "VERSION" = "$Version"
-        "PUBLISH_RELEASES" = "false"
+        "PUBLISH_RELEASES" = "$PublishRelease"
     }
 } | ConvertTo-Json
 $headers = @{
     Authorization="Bearer $env:PERSONAL_TOKEN"
 }
+
 Invoke-RestMethod -uri "$env:GITHUB_API_URL/repos/$env:GITHUB_REPOSITORY/actions/workflows/python-builder.yml/dispatches" -method POST -headers $headers -body $payload
+
 while (!$workflowToCheck) {
     Start-Sleep -seconds 40
     $workflowRuns = (Invoke-RestMethod "$env:GITHUB_API_URL/repos/$env:GITHUB_REPOSITORY/actions/runs").workflow_runs | Where-Object {$_.status -like "*progress*" -and $_.id -ne $env:GITHUB_RUN_ID}
     $workflowToCheck = $workflowRuns | Where-Object {
-        ((Invoke-RestMethod "$env:GITHUB_API_URL/repos/$env:GITHUB_REPOSITORY/actions/runs/$($_.id)/jobs").jobs.steps.name -match "$Version").Count -gt 0
+        ((Invoke-RestMethod "$env:GITHUB_API_URL/repos/$env:GITHUB_REPOSITORY/actions/runs/$($_.id)/jobs").jobs.steps.name -like "*$Version").Count -gt 0
     }
     $retries ++
     if ($retries -gt 10) {
@@ -38,14 +45,18 @@ while (!$workflowToCheck) {
         exit 1
     }
 }
+
 Write-Host "Triggered workflow with Id: $($workflowToCheck.id) , Url: $($workflowToCheck.url)"
+
 while ($workflowToCheck.status -ne "completed") {
     Start-Sleep -Seconds 120
     $workflowToCheck = Invoke-RestMethod "$env:GITHUB_API_URL/repos/$env:GITHUB_REPOSITORY/actions/runs/$($workflowToCheck.id)"
     Write-Host "Workflow run with Id: $($workflowToCheck.id) - status '$($workflowToCheck.status)'"
 }
+
 if ($workflowToCheck.conclusion -ne "success") {
     Write-Host "Triggered workflow completed with result '$($workflowToCheck.conclusion)'. Check logs: $($workflowToCheck.logs_url)"
     exit 1
 }
+
 Write-Host "Triggered workflow succeeded; Url: $($workflowToCheck.url)"
