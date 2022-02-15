a05c0e8307c1402f4e58a0d67fcc97bdd7d7bef2
Maksim Shilov <maksim.shilov@akvelon.com>
2022-02-15 15:25:39 +0300

    Improve output, try to print builds summary

--- builders/python-versions-runner.ps1
+++ builders/python-versions-runner.ps1
@@ -20,6 +20,7 @@ param(
     [switch] $PublishRelease
 )
 
+$result = @()
 $Versions | ForEach-Object -Parallel {
     $version = $_
     $payload = @{
@@ -35,7 +36,6 @@ $Versions | ForEach-Object -Parallel {
 
     Invoke-RestMethod -uri "$env:GITHUB_API_URL/repos/$env:GITHUB_REPOSITORY/actions/workflows/python-builder.yml/dispatches" -method POST -headers $headers -body $payload
 
-    $result = @()
     while ($workflowToCheck.status -ne "completed") {
         while (!$workflowToCheck) {
             Start-Sleep -seconds 40
@@ -47,8 +47,8 @@ $Versions | ForEach-Object -Parallel {
             if ($retries -gt 10) {
                 Write-Host "No workflow triggered for version $version or something went wrong with fetching its status"
                 $result += [PSCustomObject]@{
-                    Version = $version
-                    Conclusion = "failure"
+                    Version = $version;
+                    Conclusion = "failure";
                     Url = "Not run"
                 }
                 break
@@ -59,21 +59,21 @@ $Versions | ForEach-Object -Parallel {
 
         Start-Sleep -Seconds 120
         $workflowToCheck = Invoke-RestMethod "$env:GITHUB_API_URL/repos/$env:GITHUB_REPOSITORY/actions/runs/$($workflowToCheck.id)"
-        Write-Host "Workflow run with Id: $($workflowToCheck.id) - status '$($workflowToCheck.status)'"
+        Write-Host "Workflow run with Id: $($workflowToCheck.id) for version '$($version)' - status '$($workflowToCheck.status)'"
     }
     $result += [PSCustomObject]@{
-        Version = $version
-        Conclusion = $workflowToCheck.conclusion
-        Url = $workflowToCheck.logs_url
+        Version = $version;
+        Conclusion = $workflowToCheck.conclusion;
+        Url = $workflowToCheck.html_url
     }
     if ($workflowToCheck.conclusion -ne "success") {
-        Write-Host "Triggered workflow completed with result '$($workflowToCheck.conclusion)'. Check logs: $($workflowToCheck.logs_url)"
+        Write-Host "Triggered workflow for version '$($version)' completed with result '$($workflowToCheck.conclusion)'. Check logs: $($workflowToCheck.html_url)"
         break
     }
-    Write-Host "Triggered workflow succeeded; Url: $($workflowToCheck.html_url)"
+    Write-Host "Triggered workflow for version '$($version)' succeeded; Url: $($workflowToCheck.html_url)"
 }
 Write-Host "Results of triggered workflows:"
-$result
+Write-Host $result
 if ($result.Conclusion -contains "failure" -or $result.Conclusion -contains "cancelled") {
     exit 1
 }
