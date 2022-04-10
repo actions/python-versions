14d8092a1a343311d33d52a8bf4fd1723a03c7cd
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2022-04-10 11:07:32 +0530

    Update ubuntu-python-builder.psm1

--- builders/ubuntu-python-builder.psm1
+++ builders/ubuntu-python-builder.psm1
@@ -86,9 +86,9 @@ class UbuntuPythonBuilder : NixPythonBuilder {
             Execute-Command -Command "sudo apt install -y $_"
         }
 
-        if ($this.Platform -eq "linux-18.04" -and $this.Version -gt "3.11.0") {
+        if ($this.Platform -eq "linux-18.04") {
             ### On Ubuntu-1804, libgdbm-compat-dev has older modules that are no longer in libgdbm-dev
-            
+            Write-Host "Work around" 
             Execute-Command -Command "sudo add-apt-repository ppa:deadsnakes/nightly -y"
             Execute-Command -Command "sudo apt install python3.11-tk"
         }
