6727040f844865dcfa6ce96d1c8a6197afbb9965
Alice King Tai Poon <23336645+codenamedpkt@users.noreply.github.com>
2022-06-05 16:14:23 +0800

    Update nix-setup-template.sh

--- installers/nix-setup-template.sh
+++ installers/nix-setup-template.sh
@@ -49,8 +49,8 @@ fi
 chmod +x ../python $PYTHON_MAJOR $PYTHON_MAJOR_DOT_MINOR $PYTHON_MAJORMINOR python
 
 echo "Upgrading pip..."
-./python -m ensurepip
-./python -m pip install --ignore-installed pip --disable-pip-version-check --no-warn-script-location
+./python -m ensurepip --upgrade --default-pip
+./pip install --ignore-installed pip --disable-pip-version-check --no-warn-script-location
 
 echo "Create complete file"
 touch $PYTHON_TOOLCACHE_VERSION_PATH/x64.complete
