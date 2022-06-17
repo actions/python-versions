045abfcc3615cbba7c98fe000d8b359b9b406a05
Sergey Dolin <dsame@github.com>
2022-06-17 12:53:40 +0200

    Print versions

--- tests/sources/tcltk-86.py
+++ tests/sources/tcltk-86.py
@@ -1,8 +1,11 @@
 import tkinter
 import _tkinter
 
-lib = tkinter.Tk().getvar('tk_version')
 header = _tkinter.TK_VERSION
+lib = tkinter.Tk().getvar('tk_version')
+
+print('header version=' + header)
+print('lib version=' + lib)
 
 if lib != '8.6' or header != '8.6':
   exit(1)
