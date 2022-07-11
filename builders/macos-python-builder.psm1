714163cc4462da88fda15948adc73c6d116802a3
Vladimir Safonkin <vsafonkin@github.com>
2022-07-11 16:09:47 +0200

    Disable universal2 macOS builds (#175)
    * Disable universal2 macOS builds
    
    * Debug
    
    * Disable universal build and tcl test

--- builders/macos-python-builder.psm1
+++ builders/macos-python-builder.psm1
@@ -50,9 +50,10 @@ class macOSPythonBuilder : NixPythonBuilder {
         ### will never be used itself by a Github Actions runner but using a universal2 Python is the only way to build
         ### universal2 C extensions and wheels. This is supported by Python >= 3.10 and was backported to Python >=
         ### 3.9.1 and >= 3.8.10.
-        if ($this.Version -ge "3.8.10" -and $this.Version -ne "3.8.13" -and $this.Version -ne "3.9.0" ) {
-            $configureString += " --enable-universalsdk --with-universal-archs=universal2"
-        }
+        ### Disabled, discussion: https://github.com/actions/python-versions/pull/114
+        # if ($this.Version -ge "3.8.10" -and $this.Version -ne "3.8.13" -and $this.Version -ne "3.9.0" ) {
+        #     $configureString += " --enable-universalsdk --with-universal-archs=universal2"
+        # }
 
         ### OS X 10.11, Apple no longer provides header files for the deprecated system version of OpenSSL.
         ### Solution is to install these libraries from a third-party package manager,
