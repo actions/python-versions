7f200e647d76091c3fbac6479538bb37e41d62bf
mayeut <mayeut@users.noreply.github.com>
2024-05-05 13:20:10 +0200

    chore: drop support for Python 2.x builds

--- builders/ubuntu-python-builder.psm1
+++ builders/ubuntu-python-builder.psm1
@@ -37,16 +37,9 @@ class UbuntuPythonBuilder : NixPythonBuilder {
         $configureString += " --enable-shared"
         $configureString += " --enable-optimizations"
 
-        ### Compile with ucs4 for Python 2.x. On 3.x, ucs4 is enabled by default
-        if ($this.Version -lt "3.0.0") {
-            $configureString += " --enable-unicode=ucs4"
-        }
-
-        ### Compile with support of loadable sqlite extensions. Unavailable for Python 2.*
+        ### Compile with support of loadable sqlite extensions.
         ### Link to documentation (https://docs.python.org/3/library/sqlite3.html#sqlite3.Connection.enable_load_extension)
-        if ($this.Version -ge "3.2.0") {
-            $configureString += " --enable-loadable-sqlite-extensions"
-        }
+        $configureString += " --enable-loadable-sqlite-extensions"
 
         Write-Host "The passed configure options are: "
         Write-Host $configureString
@@ -60,17 +53,13 @@ class UbuntuPythonBuilder : NixPythonBuilder {
         Prepare system environment by installing dependencies and required packages.
         #>
 
-        if (($this.Version -gt "3.0.0") -and ($this.Version -lt "3.5.3")) {
-            Write-Host "Python3 versions lower than 3.5.3 are not supported"
+        if ($this.Version -lt "3.5.3") {
+            Write-Host "Python versions lower than 3.5.3 are not supported"
             exit 1
         }
 
         ### Compile with tkinter support
-        if ($this.Version -gt "3.0.0") {
-            $tkinterInstallString = "sudo apt-get install -y --allow-downgrades python3-tk tk-dev"
-        } else {
-            $tkinterInstallString = "sudo apt install -y python-tk tk-dev"
-        }
+        $tkinterInstallString = "sudo apt-get install -y --allow-downgrades python3-tk tk-dev"
 
         Execute-Command -Command "sudo apt-get update"
         Execute-Command -Command $tkinterInstallString
