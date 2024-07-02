fc3460f33a90ceec2c6a3df2a18d2c6dbe750f4c
aparnajyothi-y <147696841+aparnajyothi-y@users.noreply.github.com>
2024-07-02 11:08:19 +0530

    Update python-tests.ps1

--- tests/python-tests.ps1
+++ tests/python-tests.ps1
@@ -27,7 +27,7 @@ BeforeAll {
             $module = $regexMatch.Groups[1].Value.Trim()
             Write-Host "Failed missing modules:"
             Write-Host $module
-            if ( ($module -eq "_tkinter") -and ( [semver]"$($Version.Major).$($Version.Minor)" -ge [semver]"3.10" -and $Version.PreReleaseLabel ) ) {
+            if ( ($module -eq "_tkinter") -and ( [semver]"$($Version.Major).$($Version.Minor)" -ge [semver]"3.8" -and $Version.PreReleaseLabel ) ) {
                 Write-Host "$module $Version ignored"
             } else {
                 return 1
