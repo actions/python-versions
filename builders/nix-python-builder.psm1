8e104298292bc7f3b1013394cd1df40327177687
Haritha <73516759+HarithaVattikuti@users.noreply.github.com>
2026-02-13 11:21:12 -0600

    Modify make command for ARM architecture profiling
    Skip test_bz2 during PGO profiling for ARM on Ubuntu 22.04.

--- builders/nix-python-builder.psm1
+++ builders/nix-python-builder.psm1
@@ -114,7 +114,13 @@ class NixPythonBuilder : PythonBuilder {
         Write-Debug "make Python $($this.Version)-$($this.Architecture) $($this.Platform)"
         $buildOutputLocation = New-Item -Path $this.WorkFolderLocation -Name "build_output.txt" -ItemType File
         
-        Execute-Command -Command "make 2>&1 | tee $buildOutputLocation" -ErrorAction Continue
+        ### Skip test_bz2 during PGO profiling to work around libbz2 incompatibility
+        ### on Ubuntu 22.04 ARM runners (testDecompressorChunksMaxsize failure).
+        if (($this.Architecture -match "arm64") -and ($this.Platform -match "22\.04") -and ($this.Version -ge [semver]"3.15.0-alpha.3")) {
+            Execute-Command -Command "make PROFILE_TASK='-m test --pgo --ignore test_bz2 -j0' 2>&1 | tee $buildOutputLocation" -ErrorAction Continue
+        } else {
+            Execute-Command -Command "make 2>&1 | tee $buildOutputLocation" -ErrorAction Continue
+        }        
         Execute-Command -Command "make install" -ErrorAction Continue
 
         Write-Debug "Done; Make log location: $buildOutputLocation"
