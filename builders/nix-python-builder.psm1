26125fe9fe7008f33c78158b3b71a4b13a8ff6fd
priya-kinthali <priya-kinthali@github.com>
2026-03-11 19:05:24 +0530

    patch for 3.15.0a6

--- builders/nix-python-builder.psm1
+++ builders/nix-python-builder.psm1
@@ -149,7 +149,7 @@ class NixPythonBuilder : PythonBuilder {
 
         # Patch for 3.15.0a6: Fix test_bz2 BIG_DATA to ensure two compressed blocks
         # See: https://github.com/python/cpython/pull/145730
-        if ($this.Version -eq [semver]"3.15.0-alpha.6") {
+        if (($this.Architecture -match "arm64") -and ($this.Platform -match "22\.04") -and ($this.Version -eq [semver]"3.15.0-alpha.6")) {
             Write-Host "Applying patch for python/cpython#145730 (test_bz2 fix)..."
             Execute-Command -Command "curl -sL -o Lib/test/test_bz2.py https://raw.githubusercontent.com/python/cpython/19676e5fc28bdee8325a062a31ddeee60960cf75/Lib/test/test_bz2.py"
         }
